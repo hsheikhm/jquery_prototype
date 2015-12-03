9084316b5137f2db3be0e2edcfea4f2b427c2ef0
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-03 16:06:05 +0000

    completed styling

--- js/gitprofile.js
+++ js/gitprofile.js
@@ -4,13 +4,21 @@ $(document).ready(function() {
 
     e.preventDefault();
 
-    var access_token = ("%ACCESS_TOKEN_JQ%");
+    var access_token = 'ff0e33ae3b97a0f52096f0142ad0beba6f0a3e0f';
     var url = 'https://api.github.com/users/' + $('input.username').val() + "?access_token=" + access_token;
 
     var template = $('template').html();
 
     $.get(url, function(info) {
-      $('.container').prepend(Mustache.render(template, info));
+      console.log(info);
+      var avatar_url = info.avatar_url;
+      // var html_url = info.html_url;
+      // var login = info.login;
+      // var followers = info.followers;
+      // $('#followers').append("<p>Followers: " + public_repos + "</p>");
+      $('#repos').append("<p>Repos: " + info.public_repos + "</p>");
+
+      // $('.container').prepend(template, string);
     }).fail(function() {
       $('.container').prepend("User not found");
     }).always(function() {
